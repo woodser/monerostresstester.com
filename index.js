f82376f42a0c717489e6a69d3687ab5aa65586af
woodser <woodser@protonmail.com>
2019-09-11 19:39:20 -0400

    index.js running

--- index.js
+++ index.js
@@ -6,7 +6,8 @@
 
 "use strict"
 
-// import what you want to use
+// imports
+const assert = require("assert");
 const MoneroDaemonRpc = require("monero-javascript").MoneroDaemonRpc;
 const MoneroWalletRpc = require("monero-javascript").MoneroWalletRpc;
 const MoneroWalletLocal = require("monero-javascript").MoneroWalletLocal;
@@ -22,13 +23,35 @@ async function startApp() {
   
   // connect to monero-wallet-rpc
   let walletRpc = new MoneroWalletRpc({uri: "http://localhost:38083", user: "rpc_user", pass: "abc123"});
-  await walletRpc.openWallet("test_wallet_1", "supersecretpassword123");
+  
+  // wallet info to open or create
+  let name = "test_wallet_1";
+  let password = "supersecretpassword123";
+  let mnemonic = "nagged giddy virtual bias spying arsenic fowls hexagon oars frying lava dialect copy gasp utensils muffin tattoo ritual exotic inmate kisses either sprig sunken sprig";
+  let primaryAddress = "59aZULsUF3YNSKGiHz4JPMfjGYkm1S4TB3sPsTr3j85HhXb9crZqGa7jJ8cA87U48kT5wzi2VzGZnN2PKojEwoyaHqtpeZh";  // just for reference
+  let restoreHeight = 383338;
+  
+  // open or create a wallet
+  try {
+    await walletRpc.openWallet(name, password);
+  } catch (e) {
+    console.log(e);
+    
+    // -1 returned when the wallet does not exist or it's open by another application
+    if (e.getCode() === -1) {
+      
+      // create wallet
+      await walletRpc.createWalletFromMnemonic(name, password, mnemonic, restoreHeight);
+    } else {
+      throw e;
+    }
+  }
+  
+  // print rpc wallet balance to the console
   console.log("Wallet rpc mnemonic: " + await walletRpc.getMnemonic());
   console.log("Wallet rpc balance: " + await walletRpc.getBalance());
   
   // create a wallet from mnemonic using local wasm bindings
-  let mnemonic = "nagged giddy virtual bias spying arsenic fowls hexagon oars frying lava dialect copy gasp utensils muffin tattoo ritual exotic inmate kisses either sprig sunken sprig";
-  let primaryAddress = "59aZULsUF3YNSKGiHz4JPMfjGYkm1S4TB3sPsTr3j85HhXb9crZqGa7jJ8cA87U48kT5wzi2VzGZnN2PKojEwoyaHqtpeZh";  // just for reference
   let walletLocal = new MoneroWalletLocal({daemon: daemon, mnemonic: mnemonic});
   console.log("Local wallet address: " + await walletLocal.getPrimaryAddress());
   console.log("Local wallet height: " + await walletLocal.getHeight());
