b210e1e7ee2ea07a6dc21d19b93c7298b05b9c1b
woodser <woodser@protonmail.com>
2020-02-02 17:08:37 -0500

    testing wallet proxy in browser
    start_dev_browser copies proxy, worker, and mocha deps

--- src/tests.js
+++ src/tests.js
@@ -4,7 +4,6 @@
  * bindings.
  */
 require("monero-javascript");
-const MoneroWalletCoreProxy = require("./MoneroWalletCoreProxy");
 const assert = require("assert");
 
 /**
@@ -29,34 +28,34 @@ function runTests() {
   // test utilitiles
   new TestMoneroUtils().runTests();
   
-  // test daemon rpc
-  new TestMoneroDaemonRpc().runTests({
-    liteMode: true,  // skips some thorough but lengthy tests
-    testNonRelays: true,
-    testRelays: false, // creates and relays outgoing txs
-    testNotifications: false
-  });
-  
-  // test wallet rpc
-  new TestMoneroWalletRpc().runTests({
-    liteMode: true, // skips some lengthy but detailed tests
-    testNonRelays: true,
-    testRelays: false,
-    testNotifications: false,
-    testResets: false
-  });
-  
-  // test keys-only wallet
-  new TestMoneroWalletKeys().runTests({
-    liteMode: false,
-    testNonRelays: true,
-    testRelays: false,
-    testResets: false,
-    testNotifications: false
-  });
+//  // test daemon rpc
+//  new TestMoneroDaemonRpc().runTests({
+//    liteMode: true,  // skips some thorough but lengthy tests
+//    testNonRelays: true,
+//    testRelays: false, // creates and relays outgoing txs
+//    testNotifications: false
+//  });
+//  
+//  // test wallet rpc
+//  new TestMoneroWalletRpc().runTests({
+//    liteMode: true, // skips some lengthy but detailed tests
+//    testNonRelays: true,
+//    testRelays: false,
+//    testNotifications: false,
+//    testResets: false
+//  });
+//  
+//  // test keys-only wallet
+//  new TestMoneroWalletKeys().runTests({
+//    liteMode: false,
+//    testNonRelays: true,
+//    testRelays: false,
+//    testResets: false,
+//    testNotifications: false
+//  });
   
   // test core wallet
-  new TestMoneroWalletCore().runTests({
+  new TestMoneroWalletCoreProxy().runTests({
     liteMode: true,
     testNonRelays: true,
     testRelays: false,
