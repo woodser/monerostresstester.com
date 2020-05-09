193931b191fc3cd482a168890c6dc5887f1d1391
woodser <woodser@protonmail.com>
2020-05-09 19:58:29 -0400

    cp -R src/main/*

--- bin/build_browser_app.sh
+++ bin/build_browser_app.sh
@@ -14,7 +14,7 @@ cp node_modules/monero-javascript/dist/monero_core_keys.js browser_build/monero_
 cp node_modules/monero-javascript/dist/monero_core_keys.wasm browser_build/monero_core_keys.wasm
 cp node_modules/monero-javascript/dist/MoneroWebWorker.dist.js browser_build/MoneroWebWorker.dist.js
 cp node_modules/monero-javascript/dist/MoneroWebWorker.dist.js.map browser_build/MoneroWebWorker.dist.js.map
-cp -R src/main/ browser_build/
+cp -R src/main/* browser_build/
 
 # start server
 ./bin/start_dev_server.sh
