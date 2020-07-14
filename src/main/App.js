b452b2b95078eaafe74a9a634de190fff946541a
timetherewere <mikjosjon@gmail.com>
2020-07-14 17:38:19 -0400

    Rename "App.css" to "app.css" for consistency in capitalization.

--- src/main/App.js
+++ src/main/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import "./App.css";
+import "./app.css";
 
 import Banner from "./components/Banner.js";
 import Home from "./components/pages/Home.js";
