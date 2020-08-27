d9e926ebf3452a09437ff702bc61a798249b4c5c
timetherewere <mikjosjon@gmail.com>
2020-08-26 21:39:28 -0400

    Allow wallet restore by date rather than height.

--- src/main/App.js
+++ src/main/App.js
@@ -12,20 +12,56 @@ import Wallet from "./components/pages/Wallet.js";
 import {HashRouter as Router, Route, Switch, Redirect} from 'react-router-dom';
 
 const monerojs = require("monero-javascript");
+const LibraryUtils = monerojs.LibraryUtils;
 const MoneroWalletListener = monerojs.MoneroWalletListener;
+const MoneroWallet = monerojs.MoneroWallet;
 
 const XMR_AU_RATIO = 0.000000000001;
 
 class App extends React.Component {
   constructor(props) {
     super(props);
+    
+    // Class vars
+    this.keysModuleLoaded = false;
+    this.wasmModuleLoaded = false;
+    
+    // In order to pass "this" into the nested functions...
+    let that = this;
+    
+    //Start loading the Keys-only and Wasm wallet modules
+    LibraryUtils.loadKeysModule().then(
+      function() {
+	that.keysModuleLoaded = true;
+	alert("Keys module loaded!");
+	// Load the core module
+	LibraryUtils.loadCoreModule().then(
+	  function() {
+	    that.coreModuleLoaded = true;
+	    alert("Core module loaded!");
+	    // Load the core module
+	  }
+	).catch(
+	  function(error) {
+	    alert("Failed to load core wallet module!");
+	  } 
+	);
+      }
+    ).catch(
+      function(error) {
+	alert("Failed to load keys-only wallet module!");
+	alert("Error: " + error);
+      } 
+    );
+    
+    LibraryUtils.loadCoreModule();
 
     this.state = {
       /*
        * The mnemonic phrase (or portion thereof) that the user has typed into
        * either the "confirm" or "restore" wallet text box
        */
-      enteredPhrase: "", // The mnemonic phrase (or portion thereof) that
+      enteredPhrase: "",
       wallet: null,
       walletPhrase: "",
       phraseIsConfirmed: false,
@@ -47,11 +83,108 @@ class App extends React.Component {
     alert("Balance after conversion: " + balance * XMR_AU_RATIO);
   }
   
+  convertStringToRestoreDate(str){
+    // Make sure the string is of the format "####/##/##"
+    // Does the date have the correct number of characters? (10):
+    console.log("attempting to convert " + str + " to a restore height");
+    console.log(str + " has " + str.length + " chars; should be 10!");
+    if(str.length === 10){
+      //Attempt to divide the string into its constituent parts
+      var dateParts = str.split("/");
+      alert("This is dateparts: " + dateParts);
+      // If the result yields three strings
+      if (dateParts.length === 3){
+	// Attempt to convert each string to an integer
+	for (let i = 0; i < 3; i++){
+	  try {
+	    let n = Number(dateParts[i]);
+	    if (n === NaN) throw "Invalid date";
+	    // If the conversion worked, replace the string in the array with the number
+	    dateParts[i] = n;
+	  } catch(e){
+	    throw "Invalid date: " + e;
+	  }
+	}
+	alert("returning dateparts as: " + dateParts);
+	return dateParts;
+      } else throw "Invalid date; date should contain three numbers separated by two slashes";
+    }
+    throw "Invalid date; date should be 10 chars long";
+  }
+  
   setRestoreHeight(height){
     this.setState({
-      restoreHeight: Number(height)
+      restoreHeight: height
     });
   }
+  
+  async restoreWallet(browserHistory){
+    
+    let alertMessage = "";  
+    
+    //First, determine whether the user has typed at height, a date, or something else(invalid)
+    let height=Number(this.state.restoreHeight);
+    // If the string is NOT a valid integer, check to see if it is a date and convert accordingly:
+    if(!(height != NaN && height%10 === 0)) {
+      // Attempt to convert the string to a date in the format "YYYY/MM/DD"
+      try{
+	var dateParts = this.convertStringToRestoreDate(this.state.restoreHeight);
+	
+	// THIS IS TEMPORARY
+	// getHeightByDate is an instance function but should be a CLASS function
+	// Until this is fixed, the only way to run the function is to create a 
+	// temporary dummy wallet
+	let walletWasm = await monerojs.createWalletWasm({
+	  password: "supersecretpassword123",
+	  networkType: "stagenet",
+	  path: "",
+	  serverUri: "http://localhost:38081",
+	  serverUsername: "superuser",
+	  serverPassword: "abctesting123",
+	});
+	
+	// Attempt to convert date into a monero blockchain height:
+	height = await walletWasm.getHeightByDate(dateParts[0], dateParts[1], dateParts[2]);
+      } catch(e) {
+	alertMessage = "Well poop!\n" + e;
+      }
+    } 
+    
+    // If no errors were thrown, "height" is a valid restore height.
+    if(alertMessage !== "") {
+      alert(alertMessage);
+      return;
+    } else {
+      alert("Valid restore height!");
+    }
+    
+    let walletWasm = null;
+    try {
+      walletWasm = await monerojs.createWalletWasm({
+        password: "supersecretpassword123",
+        networkType: "stagenet",
+        path: "",
+        serverUri: "http://localhost:38081",
+        serverUsername: "superuser",
+        serverPassword: "abctesting123",
+        mnemonic: this.state.enteredPhrase,
+        restoreHeight: height
+      });
+    } catch(e) {
+      alert("Invalid mnemonic!");
+      alert("Error: " + e);
+      return;
+    }
+    browserHistory.push("/home/synchronize_wallet");
+    await this.synchronizeWallet(walletWasm);
+    this.setState ({
+      wallet: walletWasm
+    });
+  }
+
+setCurrentSyncProgress(percentDone){
+this.setState({walletSyncProgress: percentDone});
+}
   
   setEnteredPhrase(mnemonic){
     this.setState({
@@ -75,34 +208,7 @@ class App extends React.Component {
     });
   }
   
-  async restoreWallet(browserHistory){
-	    let walletWasm = null;
-	    try {
-	      walletWasm = await monerojs.createWalletWasm({
-	        password: "supersecretpassword123",
-	        networkType: "stagenet",
-	        path: "",
-	        serverUri: "http://localhost:38081",
-	        serverUsername: "superuser",
-	        serverPassword: "abctesting123",
-	        mnemonic: this.state.enteredPhrase,
-	        restoreHeight: this.state.restoreHeight
-	      });
-	    } catch(e) {
-	      alert("Invalid mnemonic!");
-	      alert("Error: " + e);
-	      return;
-	    }
-	    browserHistory.push("/home/synchronize_wallet");
-	    await this.synchronizeWallet(walletWasm);
-	    this.setState ({
-	      wallet: walletWasm
-	    });
-	  }
-  
-  setCurrentSyncProgress(percentDone){
-    this.setState({walletSyncProgress: percentDone});
-  }
+
 
   deleteWallet() {
     this.setState ({
